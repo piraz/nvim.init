a2f6669dc68cd8357ec3ed68def9f26727fae097
Flavio Garcia <piraz@candango.org>
2023-02-24 21:13:52 -0500

    feat(remaps): add clipboard copy and paste remaps
    Also add yank and paste without yanking what is selected remaps.
    It will use the a register to do that.

--- lua/piraz/remaps.lua
+++ lua/piraz/remaps.lua
@@ -13,8 +13,23 @@ nnoremap("<leader>pks", "<cmd>PackerSync<CR>")
 
 
 -- see: https://stackoverflow.com/a/73354675/2887989
+nnoremap("<leader>y", [["+y]])
 vnoremap("<leader>y", [["+y<Esc>]])
-vnoremap("<leader>Y", [["+Y<Esc>]])
+nnoremap("<leader>Y", [["+yy]])
+vnoremap("<leader>Y", [["+yy<Esc>]])
+-- TODO: Fix the leader+p in normal model, it is wayting too much
+nnoremap("<leader>p", [["+p]])
+vnoremap("<leader>p", [["+p]])
+
+-- From: https://stackoverflow.com/a/3638557
+nnoremap("<leader><leader>d", [["_dd]])
+vnoremap("<leader><leader>d", [["_dd]])
+nnoremap("<leader><leader>y", [["ay]])
+vnoremap("<leader><leader>y", [["ay]])
+nnoremap("<leader><leader>p", [["ap]])
+vnoremap("<leader><leader>p", [["ap]])
+nnoremap("<leader><leader>d", [["add]])
+vnoremap("<leader><leader>d", [["add]])
 
 -- see: https://stackoverflow.com/a/63542511/2887989
 -- By the way, to select the word under the cursor: * or g*
