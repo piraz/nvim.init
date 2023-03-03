40893b9a4706fdc7680cdfbaaa6217916fddaa66
Flavio Garcia <piraz@candango.org>
2023-03-03 13:17:05 -0500

    chore(telescope): add extra telescope remaps

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -17,6 +17,7 @@ function ColorMyPencils(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     -- Fix Gitsign black SignColumn
     vim.api.nvim_set_hl(0, "SignColumn", {bg = "none"})
 end
