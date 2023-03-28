d7c7431fdfeaa74a6303536273a6e0107b67b53f
Flavio Garcia <piraz@candango.org>
2023-03-28 00:48:20 -0400

    chore(copilot): change remaps to use cmd instead a fucntion
    This is better to check rempas in telescope

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,7 +1,7 @@
 -- vim.api.nvim_set_keymap("i", "<leader>ca", "copilot#Accept('<CR>')", {expr=true, silent=true})
-vim.keymap.set("n", "<leader>cod", function() vim.cmd("Copilot disable") end)
-vim.keymap.set("n", "<leader>coe", function() vim.cmd("Copilot enable") end)
-vim.keymap.set("n", "<leader>cos", function() vim.cmd("Copilot status") end)
+vim.keymap.set("n", "<leader>cod", "<Cmd>Copilot disable<CR>")
+vim.keymap.set("n", "<leader>coe", "<Cmd>Copilot enable<CR>")
+vim.keymap.set("n", "<leader>cos", "<Cmd>Copilot status<CR>")
 
 vim.cmd("Copilot disable")
 
