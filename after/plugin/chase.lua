0029dae4a9549f2f12130b28713af35f16e61d89
Flavio Garcia <piraz@candango.org>
2023-06-19 10:08:54 -0400

    fix(chase): test chase require before calling the setup method

--- after/plugin/chase.lua
+++ after/plugin/chase.lua
@@ -1,5 +1,12 @@
-local chase = require("chase")
-local chasepy = require("chase.python")
+local Dev = require("piraz.dev")
+local log = Dev.log
 
-chase.setup()
-chasepy.setup()
+
+local loaded, chase = pcall(require, "chase")
+if loaded then
+    local chasepy = require("chase.python")
+    chase.setup()
+    chasepy.setup()
+else
+    log.debug("chase not found")
+end
