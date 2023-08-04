67eb74d7171f15c382624d2bed2bb097f7e52141
Flavio Garcia <piraz@candango.org>
2023-08-04 13:26:19 -0400

    feat(fugitive): add git push and pull remaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gc", "<cmd>G commit<CR>")
+vim.keymap.set("n", "<leader>gpl", "<cmd>G push<CR>")
+vim.keymap.set("n", "<leader>gps", "<cmd>G push<CR>")
