889756be05e79108ac566086e7ee4682648ff739
Flavio Garcia <piraz@candango.org>
2023-10-16 22:09:24 -0400

    refactor(indent-blankline): changed config to 3 style

--- after/plugin/indent_blankline.lua
+++ after/plugin/indent_blankline.lua
@@ -1,18 +1,14 @@
 local dev = require("piraz.dev")
 local log = dev.log
 
-local loaded, indent_blankline = pcall(require, "indent_blankline")
+local loaded, indent_blankline = pcall(require, "ibl")
 if not loaded then
     log.debug("ident blankline not found")
     return
 end
 
-indent_blankline.setup {
-    -- for exampile, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = false,
-}
+indent_blankline.setup()
 
-vim.cmd("IndentBlanklineDisable")
+vim.cmd("IBLDisable")
 
-vim.keymap.set("n", "<leader>ii", "<cmd>IndentBlanklineToggle<CR>")
+vim.keymap.set("n", "<leader>ii", "<cmd>IBLToggle<CR>")
