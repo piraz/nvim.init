8e06339fe1dfb448499e7c3db3672ab04f628a78
Flavio Garcia <piraz@candango.org>
2024-12-09 14:51:55 -0500

    feat(telescope): bump telescope to 0.1.8
    Also add nvim-telescope/telescope-fzf-native.nvim as a telescope
    dependency.

--- lua/piraz/plugins.lua
+++ lua/piraz/plugins.lua
@@ -24,8 +24,11 @@ local plugins = {
 
     -- Telescope
     {
-        "nvim-telescope/telescope.nvim", tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" }
+        "nvim-telescope/telescope.nvim", tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        }
     },
 
     -- Treesitter plugins
