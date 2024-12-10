575f356193c608e3da7227ca8dddc41ec7de09f6
Flavio Garcia <piraz@candango.org>
2024-12-10 00:12:50 -0500

    refactor(telescope): move telescope config to the lazy structure

--- lua/piraz/plugins/telescope.lua
+++ lua/piraz/plugins/telescope.lua
@@ -5,6 +5,23 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-        }
+        },
+        config = function ()
+            local builtin = require("telescope.builtin")
+
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
+            vim.keymap.set("n", "<leader>fc", builtin.colorscheme, {})
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>fk", builtin.keymaps, {})
+            vim.keymap.set("n", "<leader>fq", builtin.quickfix, {})
+            vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
+
+            vim.keymap.set("n", "<leader>gb", builtin.git_bcommits, {})
+            vim.keymap.set("n", "<leader>gh", builtin.git_commits, {})
+            vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
+        end
     },
 }
