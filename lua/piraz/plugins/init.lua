e08ae95a24d4d411621097d5a46fbea3c9866c72
Flavio Garcia <piraz@candango.org>
2024-12-10 00:26:05 -0500

    refactor(trouble): move config to the lazy structure

--- lua/piraz/plugins/init.lua
+++ lua/piraz/plugins/init.lua
@@ -20,15 +20,6 @@ return {
             -- refer to the configuration section below
         }
     },
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-    },
     { "raimon49/requirements.txt.vim" },
     { "fatih/vim-go" },
 }
---
+++ lua/piraz/plugins/trouble.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        config = function ()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+                icons = true,
+                fold_open = "v", -- icon used for open folds
+                fold_closed = ">", -- icon used for closed folds
+                indent_lines = false, -- add an indent guide below the fold icons
+                signs = {
+                    -- icons / text used for a diagnostic
+                    error = "error",
+                    warning = "warn",
+                    hint = "hint",
+                    information = "info"
+                },
+                use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+            }
+        end
+    },
+}
