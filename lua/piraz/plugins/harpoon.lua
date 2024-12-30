fa52cdfc2cad2e2f98d9fe1e61cab6e08945774d
Flavio Garcia <piraz@candango.org>
2024-12-30 16:25:50 -0500

    feat(harpoon): crank harpoon list to 7 items

--- lua/piraz/plugins/harpoon.lua
+++ lua/piraz/plugins/harpoon.lua
@@ -16,6 +16,8 @@ return {
             vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
             vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
             vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
+            vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
+            vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
         end,
     },
 }
