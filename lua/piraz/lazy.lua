238ddc95d66714c998f5322c3c6de4826bc66073
Flavio Garcia <piraz@candango.org>
2025-03-06 14:18:45 -0500

    chore(lazy): set change_detection and checker to false

--- lua/piraz/lazy.lua
+++ lua/piraz/lazy.lua
@@ -26,7 +26,7 @@ require("lazy").setup({
     spec = {
         { import = "piraz/plugins" },
     },
-    checker = { enabled = true },
+    change_detection = { notify = false },
     dev = {
         path = "~/source/candango",
     },
