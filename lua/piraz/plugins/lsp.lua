78c0df8e3baea4a4d0270509d6058a030e689942
Flavio Garcia <piraz@candango.org>
2025-07-11 17:44:23 -0400

    chore(lsp): set lua snip version to v2.*

--- lua/piraz/plugins/lsp.lua
+++ lua/piraz/plugins/lsp.lua
@@ -252,6 +252,7 @@ return {
     -- Snippets
     {
         "L3MON4D3/LuaSnip",
+        version = "v2.*",
         build="make install_jsregexp",
     }, -- Required
     { "rafamadriz/friendly-snippets" },
