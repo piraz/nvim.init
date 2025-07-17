b9bda8f8be3b0f1fedfb54ae13ab2ef5747ac12d
Flavio Garcia <piraz@candango.org>
2025-07-17 00:11:13 -0400

    feat(copilot): add copilot plugin configuration

--- lua/piraz/plugins/copilot.lua
+++ lua/piraz/plugins/copilot.lua
@@ -1,8 +1,34 @@
 return {
-    -- "zbirenbaum/copilot.lua",
-    -- cmd = "Copilot",
-    -- event = "InsertEnter",
-    -- config = function()
-    --     require("copilot").setup({})
-    -- end,
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = { enabled = false },
+                panel = { enabled = false },
+                filetypes = {
+                    gitcommit = true,
+                    go = true,
+                    python = true,
+                    sql = true,
+                    html = true,
+                    yaml = true,
+                    xml = true,
+                    ["requirements.txt"] = true,
+                    ["package.json"] = true,
+                    ["pyproject.toml"] = true,
+                },
+            })
+        end,
+    },
+    {
+        "AndreM222/copilot-lualine"
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        config = function ()
+            require("copilot_cmp").setup()
+        end
+    }
 }
--- lua/piraz/plugins/lualine.lua
+++ lua/piraz/plugins/lualine.lua
@@ -27,7 +27,7 @@ return {
                     lualine_a = {"mode"},
                     lualine_b = {"branch", "diff", "diagnostics"},
                     lualine_c = {{"filename", path = 3}},
-                    lualine_x = {"encoding", "fileformat", "filetype"},
+                    lualine_x = {"copilot", "encoding", "fileformat", "filetype"},
                     lualine_y = {"progress"},
                     lualine_z = {"location"}
                 },
