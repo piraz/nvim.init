ce4a72c0a114ec2023f0e1148403d37cc0b2281b
Flavio Garcia <piraz@candango.org>
2025-08-11 15:41:00 -0400

    chore(remaps): check if the file is lua into the leader so remap

--- lua/piraz/remaps.lua
+++ lua/piraz/remaps.lua
@@ -66,7 +66,7 @@ vim.keymap.set("n", "<leader>ee", "<cmd>Ex<CR>", { desc = "Open file explorer" }
 vim.keymap.set("n", "<leader>ls", "<cmd>ls<CR>", { desc = "List buffers" })
 vim.keymap.set("n", "<leader>ln" , function() vim.opt.number = not vim.opt.number:get() end, {desc = "Toggle opt number"})
 vim.keymap.set("n", "<leader>lr" , function() vim.opt.relativenumber = not vim.opt.relativenumber:get() end, {desc = "Toggle opt relativenumber"})
-vim.keymap.set("n", "<leader>so", function() vim.cmd("luafile %") end, {desc = "Souce file"})
+vim.keymap.set("n", "<leader>so", function() if vim.bo.filetype == "lua" then vim.cmd("so %") end end, {desc = "Souce file"})
 vim.keymap.set("n", "<leader>x", run_file, {desc = "Run file"})
 vim.keymap.set("n", "<leader>mzu", "<cmd>MasonUpdate<CR>", { desc = "Mason Update" })
 vim.keymap.set("n", "<leader>lzs", "<cmd>Lazy sync<CR>", { desc = "Lazy sync" })
