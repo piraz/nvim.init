38c3fc282619645ad06b55992333b9c2477be94c
Flavio Garcia <piraz@candango.org>
2025-08-12 09:33:23 -0400

    chore(trouble): use new trouble configuration

--- lua/piraz/plugins/trouble.lua
+++ lua/piraz/plugins/trouble.lua
@@ -1,30 +1,37 @@
 return {
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
         },
-        config = function ()
-            require("trouble").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-                icons = true,
-                fold_open = "v", -- icon used for open folds
-                fold_closed = ">", -- icon used for closed folds
-                indent_lines = false, -- add an indent guide below the fold icons
-                signs = {
-                    -- icons / text used for a diagnostic
-                    error = "error",
-                    warning = "warn",
-                    hint = "hint",
-                    information = "info"
-                },
-                use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
-            }
-        end
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
     },
 }
